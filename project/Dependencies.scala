0ffde877aa59aa8a0f2f2b3013153d112475510e
Cédric Chantepie <cchantep@users.noreply.github.com>
2022-01-04 20:50:57 +0100

    Update scalafmt setup + Macro facade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ object Dependencies {
   // scalaVersion needs to be kept in sync with travis-ci
   val Scala212 = "2.12.15"
   val Scala213 = "2.13.7"
-  val Scala3   = "3.0.2"
+  val Scala3   = "3.1.2-RC1-bin-20211222-c94b333-NIGHTLY"
 }
